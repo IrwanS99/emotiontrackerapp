0a0b095fdd23479b64a967ec6b4b78a148bd8f51
Irwan Sanjaya <irwansanjaya7999@gmail.com>
2024-12-19 14:52:20 +0700

    update flutter

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,14 @@
 import 'package:emotiontrackerapp/presentation/screens/Onboarding_Screen.dart';
 import 'package:flutter/material.dart';
 
-
-
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: OnboardingScreen(),
+      home: OnboardingScreen(),
     );
   }
 }
